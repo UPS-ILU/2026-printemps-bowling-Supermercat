0d5ee5afac8d34c6148a31a345058f41d5ec4f4c
Jihao Cai <cwj4888a@ensfsi.onmicrosoft.com>
2026-04-17 14:51:18 +0200

    test 1 passed

---
+++ src/printempsbowlingSupermercat/Game.java
@@ -0,0 +1,8 @@
+package printempsbowlingSupermercat;
+
+public class Game {
+	public String next() {
+        return "1";
+    }
+
+}
--- test/printempsbowlingSupermercat/GameTest.java
+++ test/printempsbowlingSupermercat/GameTest.java
@@ -1,6 +1,7 @@
 package printempsbowlingSupermercat;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 
 public class GameTest {
